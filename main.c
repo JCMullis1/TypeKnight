d7d7ffbd80c853d29a47855ecb6a975c158f11fa
user <joncalebmullis@gmail.com>
2026-05-26 12:46:14 -0500

    Adding game loop

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 int randword(int arrsize) {
 	int result = (rand() % arrsize);
@@ -10,6 +11,7 @@ int randword(int arrsize) {
 }
 
 void typingtest(int strlen, char A[][strlen], size_t size) {
+	printf("\033[H\033[2J");
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < strlen; j++) {
 			printf("%c", A[i][j]);
@@ -20,7 +22,26 @@ void typingtest(int strlen, char A[][strlen], size_t size) {
 }
 
 int main(int argc, char *argv[]) {
-	if (SDL_Init(SDL_INIT_AUDIO) != 0) {
+	// NEED** Condition to switch game to headless
+	// mode in the case of no graphics driver
+
+//	if ( SDL_Init(SDL_INIT_VIDEO ) != 0) {
+//		SDL_Log("Unable to initialize SDL Video: %s", SDL_GetError());
+//		printf("Attempting headless mode...\n");
+//		putenv("SDL_VIDEODIRVER=dummy");
+//		if ( SDL_Init(SDL_INIT_VIDEO ) != 0) {
+//			SDL_Log("Unable to run in initialize in headless mode: %s", SDL_GetError());
+//			return 1;
+//		}	
+//	} 
+
+	putenv("SDL_VIDEODRIVER=dummy");
+	if ( SDL_Init(SDL_INIT_VIDEO ) != 0) {
+		SDL_Log("Couldn't run in headless mode: %s", SDL_GetError());
+		return 1;
+	}	
+
+	if ( SDL_Init( SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER ) != 0) {
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
 	}
@@ -37,9 +58,33 @@ int main(int argc, char *argv[]) {
 		{"dog"}
 	}; // size in bytes is 63	
 
-//	printf("%s\n", word[randword(sizeof(word)/sizeof(*word))]);
 
-	typingtest(sizeof(*word), word, sizeof(word)/sizeof(*word));
+
+// 	typingtest(sizeof(*word), word, sizeof(word)/sizeof(*word));
+	bool running = true;
+	SDL_Event event;	
+
+	// main game loop
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_QUIT:
+					running = false;	
+					break;
+				case SDL_KEYDOWN:
+					printf("a key has been pressed\n");
+					switch (event.key.keysym.sym) {
+						case SDLK_ESCAPE:
+							running = false;
+							break;	
+						default:
+							break;
+					}		
+				default:
+					break;
+			}	
+		}
+	}
 
 	return 0;
 
